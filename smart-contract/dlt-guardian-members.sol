7c866b62d2c54022fb67a5d133a047b54b43aeca
Fabio Melloni <fabio.melloni@futuresolutionservices.com>
2018-05-16 10:34:14 +0200

    Add events for organisation list changes

--- smart-contract/dlt-guardian-members.sol
+++ smart-contract/dlt-guardian-members.sol
@@ -26,6 +26,12 @@ contract DLT_guardian_membership is Ownable {
         _;
     }
 
+    event CreateOrganisation(address, Organisation);
+    event DeactivateOrganisation(address);
+    event ReactivateOrganisation(address);
+    event ChangeURL(address, string);
+    event ChangeDescription(address, string);
+
     // Methods that can be called by admin (contract owner)
 
     function admin_setWalletAccount(address _walletAccount) public onlyOwner { // Allows owner to change the address that receives membeship fees
@@ -40,10 +46,12 @@ contract DLT_guardian_membership is Ownable {
     function admin_deactivateMemberAccount(address _memberAddress) public onlyOwner { // Deactivate account of a registered member (for violating conditions)
         require(members[_memberAddress].registered);
         members[_memberAddress].active = false;
+        emit DeactivateOrganisation(_memberAddress);
     }
     function admin_reactivateMemberAccount(address _memberAddress) public onlyOwner { // Reactivate a previously deactivated account
         require(members[_memberAddress].registered);
         members[_memberAddress].active = true;
+        emit ReactivateOrganisation(_memberAddress);
     }
     function admin_sweepEth() external onlyOwner { // Sweeps ether incorrectly sent to this contract to wallet account (should not be necessary)
         // solium-disable-next-line security/no-send
@@ -54,13 +62,16 @@ contract DLT_guardian_membership is Ownable {
 
     function member_changeOrgWebsite(string _newWebsite) public onlyActiveMember {
         members[msg.sender].website = _newWebsite;
+        emit ChangeURL(msg.sender, _newWebsite);
     }
 
     function member_changeOrgDescription(string _newDescription) public onlyActiveMember {
         members[msg.sender].description = _newDescription;
+        emit ChangeDescription(msg.sender, _newDescription);
     }
     function member_cancelMembership() public onlyActiveMember { // Allows a member to deactivate their own organisation
         members[msg.sender].active = false;
+        emit DeactivateOrganisation(msg.sender);
     }
 
     // Methods that can be called by anyone
@@ -73,12 +84,15 @@ contract DLT_guardian_membership is Ownable {
         require(walletAccount.send(msg.value)); // Send registration fee to wallet account, check for success
 
         memberAddresses.push(msg.sender); // Add address to registered list
-        members[msg.sender] = Organisation({
+
+        Organisation memory newOrganisation = Organisation({
             name: _orgName,
             website: _orgWebsite,
             description: _orgDescription,
             registered: true,
             active: true
         });
+        members[msg.sender] = newOrganisation;
+        emit CreateOrganisation(msg.sender, newOrganisation);
     }
 }
